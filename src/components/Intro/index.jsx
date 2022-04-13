98181739431f049a05e8062a823269cc3498a967
Your Name <you@example.com>
2022-04-13 17:07:02 +0100

    intro page started

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -4,8 +4,14 @@ import "./index.css";
 export default function Intro() {
   return (
     <div className="intro" id="intro">
-      <div className="left"></div>
-      <div className="right"></div>
+      <div className="container">
+        <h2>Hi, my name is</h2>
+        <h1>Michael Ovie-Afabor</h1>
+        <h3>I'm a Full-Stack Developer</h3>
+        <div>
+          <button>View Work</button>
+        </div>
+      </div>
     </div>
   );
 }
