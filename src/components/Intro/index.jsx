6f9e5d744725d2ed9a657a9e059afea5a11210c4
Your Name <you@example.com>
2022-04-21 07:45:26 +0100

    project page started

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./index.css";
+import { init } from "ityped";
 
-export default function Intro() {
+export default function Intro({ url }) {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      backDelay: 300,
+      backSpeed: 100,
+      strings: [
+        " Full-Stack Developer",
+        " React Designer",
+        " PERN Stack Developer",
+      ],
+    });
+  }, []);
   return (
     <div className="intro" id="intro">
       <div className="container">
         <h2>Hi, my name is</h2>
         <h1>Michael Ovie-Afabor</h1>
-        <h3>I'm a Full-Stack Developer</h3>
+        <h3>
+          I'm a<span ref={textRef}> </span>
+        </h3>
         <div>
-          <button>View Work</button>
+          <button url="#projects">View Work</button>
         </div>
       </div>
     </div>
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -2,5 +2,22 @@ import React from "react";
 import "./index.css";
 
 export default function Projects() {
-  return <div className="projects" id="projects"></div>;
+  return (
+    <div className="projects" id="projects">
+      <h1>Projects</h1>
+      <ul>
+        <li>Boot Camper Portal</li>
+        <li>My Tribe Mobile App</li>
+        <div className="container">
+          <div className="item">
+            <img
+              src="https://designshack.net/wp-content/uploads/placeholder-image.png"
+              alt="Week 9 Project"
+            />
+            <h3>Boot Camper Portal</h3>
+          </div>
+        </div>
+      </ul>
+    </div>
+  );
 }
