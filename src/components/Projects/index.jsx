7ff9420a1ec6ba39b38d709b765f480c2c402638
Your Name <you@example.com>
2022-08-15 19:26:14 +0100

    map not working

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,39 +1,68 @@
 import React from "react";
 import "./projects.scss";
+import { data } from "../../data";
 
 export default function Projects() {
+  const data = [
+    {
+      id: "1",
+      icon: "assets/web.png",
+      image: "assets/project1.png",
+      title: "Bootcamper Portal App",
+      desc: "This app was created as a group to improve the experience of fellow bootcampers. We created an app where bootcampers can provide better feedback to each session as well as get additional links for topics that have been covered",
+      tech: "React Posgres",
+    },
+
+    {
+      id: "2",
+      icon: "assets/mobile.png",
+      image: "assets/project2.png",
+      title: "MyTribe Family App,",
+      desc: "The MyTribe app was created to organise family chores and responsibilities. This was my first experience using React Native",
+      tech: "React Native Firebase",
+    },
+    {
+      id: "3",
+      icon: "assets/mobile.png",
+      image: "assets/project3.png",
+      title: "Weather App",
+      desc: "This app was created to collect data from a Weather API. I mainly focused on experimenting with different styling and using APIs to fetch data ",
+      tech: "React",
+    },
+    {
+      id: "4",
+      icon: "assets/web.png",
+      image: "assets/project4.png",
+      title: "News App",
+      desc: "This app was created to collect data from a News API. This was also another opportunity to focus on styling and fetching data",
+    },
+  ];
+
   return (
     <div className="projects" id="projects">
-      {/* <h1>Projects</h1>
-      <ul>
-        <li>Boot Camper Portal</li>
-        <li>My Tribe Mobile App</li>
-      </ul> */}
       <div className="slider">
+        {/* {data.map((d) => { */}
         <div className="container">
           <div className="item">
             <div className="left">
               <div className="leftContainer">
                 <div className="imgContainer">
-                  <img src="assets/web.png" alt="" />
+                  <img src="assets/mobile.png" alt="" />
                 </div>
-                <h2>Bootcamper Portal App</h2>
-                <p>
-                  This app was created as a group to improve the experience of
-                  fellow bootcampers. We created an app where bootcampers can
-                  provide better feedback to each session as well as get
-                  additional links for topics that have been covered.
-                </p>
+                <h2>TITLE</h2>
+                <p>DESCRIPTOON</p>
                 <h3>Technologies Used:</h3>
-                <span>React Postgres</span>
+                <span>TECH</span>
               </div>
             </div>
             <div className="right">
-              <img src="assets/project1.png" alt="" />
+              <img src="assets/project4.png" alt="" />
             </div>
           </div>
         </div>
+        {/* })} */}
       </div>
+
       <img src="assets/arrow.png" className="arrow left" alt="" />
       <img src="assets/arrow.png" className="arrow right" alt="" />
     </div>
--- src/data.js
+++ src/data.js
@@ -9,9 +9,22 @@ export const data = [
 
   {
     icon: "assets/mobile.png",
-    image: "assets/",
+    image: "assets/project2.png",
     title: "MyTribe Family App,",
     desc: "The MyTribe app was created to organise family chores and responsibilities. This was my first experience using React Native",
     tech: "React Native Firebase",
+  },
+  {
+    icon: "assets/mobile.png",
+    image: "assets/project3.png",
+    title: "Weather App",
+    desc: "This app was created to collect data from a Weather API. I mainly focused on experimenting with different styling and using APIs to fetch data ",
+    tech: "React",
+  },
+  {
+    icon: "assets/web.png",
+    image: "assets/project4.png",
+    title: "News App",
+    desc: "This app was created to collect data from a News API. This was also another opportunity to focus on styling and fetching data",
   },
 ];
