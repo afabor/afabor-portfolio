e256471bd188820966c0a8d186d095c8388fd15d
Your Name <you@example.com>
2022-08-16 00:34:36 +0100

    map working but not as intended

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -9,8 +9,8 @@ export default function Projects() {
       icon: "assets/web.png",
       image: "assets/project1.png",
       title: "Bootcamper Portal App",
-      desc: "This app was created as a group to improve the experience of fellow bootcampers. We created an app where bootcampers can provide better feedback to each session as well as get additional links for topics that have been covered",
-      tech: "React Posgres",
+      desc: "This app was created as a group to improve the experience of fellow bootcampers. This my first group project done between four people",
+      tech: "PERN",
     },
 
     {
@@ -35,32 +35,33 @@ export default function Projects() {
       image: "assets/project4.png",
       title: "News App",
       desc: "This app was created to collect data from a News API. This was also another opportunity to focus on styling and fetching data",
+      tech: "React",
     },
   ];
 
   return (
     <div className="projects" id="projects">
       <div className="slider">
-        {/* {data.map((d) => { */}
         <div className="container">
-          <div className="item">
-            <div className="left">
-              <div className="leftContainer">
-                <div className="imgContainer">
-                  <img src="assets/mobile.png" alt="" />
+          {data.map((data) => (
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <img src={data.icon} alt="" />
+                  </div>
+                  <h2>{data.title}</h2>
+                  <p>{data.desc}</p>
+                  <h3>Technologies Used:</h3>
+                  <span>{data.tech}</span>
                 </div>
-                <h2>TITLE</h2>
-                <p>DESCRIPTOON</p>
-                <h3>Technologies Used:</h3>
-                <span>TECH</span>
+              </div>
+              <div className="right">
+                <img src={data.image} alt="" />
               </div>
             </div>
-            <div className="right">
-              <img src="assets/project4.png" alt="" />
-            </div>
-          </div>
+          ))}
         </div>
-        {/* })} */}
       </div>
 
       <img src="assets/arrow.png" className="arrow left" alt="" />
