6114a495888e7618f8fad2f0f2dad0755878952e
Your Name <you@example.com>
2022-08-18 15:34:06 +0100

    email js working

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,18 +1,35 @@
 import React, { useState } from "react";
+import { useRef } from "react";
 import "./contact.scss";
+import emailjs from "@emailjs/browser";
 
 export default function Contact() {
+  const formRef = useRef();
+
   const [message, setMessage] = useState(false);
 
-  function handleSubmit(e) {
+  const sendEmail = (e) => {
     e.preventDefault();
-    setMessage(true);
-  }
+
+    emailjs
+      .sendForm(
+        "service_vg5kahk",
+        "template_gpnz6de",
+        formRef.current,
+        "zgnYDXwm9p3rSu4u7"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <div className="contact" id="contact">
-      <div className="wrapper"></div>
-      <div className="left">
-        <h1>Keep in touch.</h1>
+      <div className="wrapper">
         <div className="info">
           <div className="item">
             <img src="assets/github.png" className="icon" alt="" />
@@ -25,13 +42,17 @@ export default function Contact() {
           </div>
         </div>
       </div>
+      <div className="left">
+        <h1>Keep in touch.</h1>
+      </div>
       <div className="right">
         <p className="desc">
           <b>Let's work together.</b> I will be sure to respond as soon as
           possible. Hope to hear from you soon.
         </p>
-        <form onSubmit={handleSubmit}>
+        <form ref={formRef} onSubmit={sendEmail}>
           <input type="text" placeholder="Name" name="user_name" />
+          <input type="text" placeholder="Subject" name="user_subject" />
           <input type="text" placeholder="Email" name="user_email" />
           <textarea rows={5} placeholder="Message" name="message"></textarea>
           <button type="submit">send</button>
