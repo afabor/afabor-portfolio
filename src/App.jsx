f6911f29d3fff6cab9b2012fde8cb2ad72bcdff4
Your Name <you@example.com>
2022-08-19 00:10:51 +0100

    light/dark mode started

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
+import { useState } from "react";
 import NavBar from "./components/NavBar";
 import Intro from "./components/Intro";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import "./App.css";
+import Toggle from "./components/Toggle";
 
 function App() {
+  const [dark, setDark] = useState(false);
   return (
     <div className="App">
+      <Toggle setDark={setDark} />
       <NavBar />
       <div className="sections">
-        <Intro />
-        <Projects />
-        <Contact />
+        <Intro dark={dark} />
+        <Projects dark={dark} />
+        <Contact dark={dark} />
       </div>
     </div>
   );
--- src/components/Toggle/index.jsx
+++ src/components/Toggle/index.jsx
@@ -4,8 +4,9 @@ import "./toggle.scss";
 export default function Toggle() {
   return (
     <div className="toggle">
-      <img src="assets/sun.png" alt="sun" />
-      <img src="assets/moon.png" alt="moon" />
+      <img src="assets/sun.png" alt="sun" className="icon" />
+      <img src="assets/moon.png" alt="moon" className="icon" />
+      <div className="toggleButton"></div>
     </div>
   );
 }
