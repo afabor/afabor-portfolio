0cbe5d7cc95bf4cc3a53b473345ce988ef58786b
Your Name <you@example.com>
2024-03-26 23:07:30 +0000

    projects page updated, sneaklean app added to data list

--- src/sliderData.js
+++ src/sliderData.js
@@ -14,7 +14,7 @@ export const data = [
     image: "assets/project2.png",
     title: "MyTribe Family App",
     desc: "The MyTribe app was created to organise family chores and responsibilities. This was my first experience using React Native.",
-    tech: "React Native Firebase",
+    tech: "React Native, Firebase",
   },
   {
     id: "3",
@@ -31,5 +31,13 @@ export const data = [
     title: "News App",
     desc: "This app was created to collect data from a News API. This was also another opportunity to focus on styling and fetching data.",
     tech: "React",
+  },
+  {
+    id: "5",
+    icon: "assets/web.png",
+    image: "assets/sneaklean.png",
+    title: "SneaKlean Milestone App",
+    desc: "My milestone project with Code Institute was this web page based on a sneaker cleaning service. This was a great chance for me to experiment with Bootstrap",
+    tech: "HTML, CSS, Bootstrap",
   },
 ];
